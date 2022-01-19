2d344f2ccbb8a1458ec1b3133936eb38d6f99645
Danielle Brinkley <daniellebrinkley.fs@gmail.com>
2022-01-19 06:33:31 -0700

    add actions types

---
+++ client/src/reducers/postReducer.js
@@ -0,0 +1,10 @@
+
+
+
+const initialState = {
+    posts: [
+        {id: '1234', title: 'New Post 1', body: 'Checkout this post'},
+        {id: "5678", title: 'New Post 2', body: 'Checkout this post'},
+        {id: '9879', title: 'New Post 3', body: 'Checkout this post'}
+    ]
+}
