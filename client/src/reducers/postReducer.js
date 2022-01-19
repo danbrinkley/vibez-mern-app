71fcfb8e1ad2d6f1ee4dbf0608d731ac3d79f9d6
Danielle Brinkley <daniellebrinkley.fs@gmail.com>
2022-01-19 06:37:07 -0700

    add get_posts to postreducer

--- client/src/reducers/postReducer.js
+++ client/src/reducers/postReducer.js
@@ -1,4 +1,4 @@
-
+import { GET_POSTS, ADD_POSTS, DELETE_POSTS } from '../actions/types'
 
 
 const initialState = {
@@ -7,4 +7,16 @@ const initialState = {
         {id: "5678", title: 'New Post 2', body: 'Checkout this post'},
         {id: '9879', title: 'New Post 3', body: 'Checkout this post'}
     ]
+}
+
+
+export default function(state = initialState, action) {
+    switch(action.type) {
+       case GET_POSTS:
+           return {
+               ...state
+           }
+           default:
+               return state;
+    }
 }
