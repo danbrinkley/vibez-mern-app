190c5eeb19081640fcf4eed0133cb084a2f764e2
Danielle Brinkley <daniellebrinkley.fs@gmail.com>
2022-01-18 20:32:54 -0700

    connect mmongodb

---
+++ models/index.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const db = mongoose.connection;
+const configs = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+   
+};
+
+mongoose.connect(process.env.DB_URL, configs);
+
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Connected to Vibez");
+});
+
+module.exports = {
+    User: require("./User"),
+    Post: require("./Post"),
+};
