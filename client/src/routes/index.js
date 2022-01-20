2be7687a3f824c1954a924777e0e40ed90e0af2d
Danielle Brinkley <daniellebrinkley.fs@gmail.com>
2022-01-20 06:49:44 -0700

    update routes to use Router

--- client/src/routes/index.js
+++ client/src/routes/index.js
@@ -1,17 +1,17 @@
 import React from "react";
-import { Switch, Route } from "react-router-dom";
+import { Router, Route } from "react-router";
 // import SignupPage from "../components/pages/SignupPage";
 // import LoginPage from "../components/pages/LoginPage";
 import HomePage from "../pages/HomePage";
 import ProfilePage from "../pages/ProfilePage";
 
 const Routes = () => (
-  <Switch>
+  <Router>
      {/* <Route exact path="/register" render={(props) => <SignupPage {...props} />}/>
       <Route exact path="/auth/login" render={(props) => <LoginPage {...props} />} /> */}
         <Route exact path="/profile" render={(props) => <ProfilePage {...props} />}/> 
           <Route exact path="/home" render={(props) => <HomePage {...props} />} />
-  </Switch>
+  </Router>
 );
 
 export default Routes;
