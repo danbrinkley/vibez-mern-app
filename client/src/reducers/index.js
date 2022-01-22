6fca02cd304c79ceb667f1694c57fc53719567e7
Danielle Brinkley <daniellebrinkley.fs@gmail.com>
2022-01-22 11:23:56 -0700

    add errorReducers

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,9 +1,13 @@
 import { combineReducers } from "redux";
 import postReducer from './postReducer';
+import authReducer from './authReducer';
+import errorReducer from './errorReducer';
 
 
 
 
 export default combineReducers({
-    post: postReducer
+    post: postReducer, 
+    error: errorReducer,
+    auth: authReducer
 });
