3d068e78039902eeb15a1537307e3ca53ca86262
Danielle Brinkley <daniellebrinkley.fs@gmail.com>
2022-02-17 04:14:54 -0700

    push to heroku

--- src/components/ChatBox/index.js
+++ src/components/ChatBox/index.js
@@ -1,36 +1,36 @@
-import React from 'react';
-import { StreamChat } from 'stream-chat';
-import { Chat, Channel, ChannelHeader, MessageInput, MessageInputSmall, VirtualizedMessageList, Window } from 'stream-chat-react';
+// import React from 'react';
+// import { StreamChat } from 'stream-chat';
+// import { Chat, Channel, ChannelHeader, MessageInput, MessageInputSmall, VirtualizedMessageList, Window } from 'stream-chat-react';
 
-// import 'stream-chat-react/dist/css/index.css';
+// // import 'stream-chat-react/dist/css/index.css';
 
-const chatClient = StreamChat.getInstance('achdd329h4sq');
-const userToken = '5pr74vkd4u6mzhexrq7u6v8cys37nv7dqepxhr4vjcstac932xp2s29chv5wqxdr';
+// const chatClient = StreamChat.getInstance('achdd329h4sq');
+// const userToken = '5pr74vkd4u6mzhexrq7u6v8cys37nv7dqepxhr4vjcstac932xp2s29chv5wqxdr';
 
-chatClient.connectUser(
-  {
-    id: 'dbrink',
-    name: 'rocc24',
-    image: 'https://getstream.io/random_png/?id=falling-silence-0&name=falling',
-  },
-  userToken,
-);
+// chatClient.connectUser(
+//   {
+//     id: 'dbrink',
+//     name: 'rocc24',
+//     image: 'https://getstream.io/random_png/?id=falling-silence-0&name=falling',
+//   },
+//   userToken,
+// );
 
-const channel = chatClient.channel('livestream', 'vibez', {
-  image: 'https://goo.gl/Zefkbx',
-  name: 'Welcome to Vibez',
-});
+// const channel = chatClient.channel('livestream', 'vibez', {
+//   image: 'https://goo.gl/Zefkbx',
+//   name: 'Welcome to Vibez',
+// });
 
-const ChatBox = () => (
-  <Chat client={chatClient} theme='livestream dark'>
-    <Channel channel={channel}>
-      <Window>
-        <ChannelHeader live />
-        <VirtualizedMessageList />
-        <MessageInput Input={MessageInputSmall} focus />
-      </Window>
-    </Channel>
-  </Chat>
-);
+// const ChatBox = () => (
+//   <Chat client={chatClient} theme='livestream dark'>
+//     <Channel channel={channel}>
+//       <Window>
+//         <ChannelHeader live />
+//         <VirtualizedMessageList />
+//         <MessageInput Input={MessageInputSmall} focus />
+//       </Window>
+//     </Channel>
+//   </Chat>
+// );
 
-export default ChatBox;
+// export default ChatBox;
