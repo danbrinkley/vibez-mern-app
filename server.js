74dea868b5b8409283acc5e64df7bce0d15905ec
Danielle Brinkley <daniellebrinkley.fs@gmail.com>
2022-02-24 03:57:06 -0700

    deploy to heroku

--- server.js
+++ server.js
@@ -4,9 +4,11 @@ const http = require("http");
 const cors = require("cors");
 const { Server } = require("socket.io");
 app.use(cors());
+app.use(express.static(path.join(__dirname, "./client/build")));
+app.use(express.static("public"));
 
 const server = http.createServer(app);
-
+const PORT = process.env.PORT || 5000;
 const io = new Server(server, {
   cors: {
     origin: "http://localhost:3000",
@@ -31,6 +33,10 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(3001, () => {
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build", "index.html"));
+});
+
+server.listen(PORT, () => {
   console.log("SERVER RUNNING");
 });
