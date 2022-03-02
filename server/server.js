6e18b04a948fe43d903509ad2dc5066b269bb52c
Danielle Brinkley <daniellebrinkley.fs@gmail.com>
2022-03-02 02:39:42 -0700

    get ready to deploy

--- server/server.js
+++ server/server.js
@@ -2,9 +2,9 @@ const express = require("express");
 const app = express();
 const http = require("http");
 const cors = require("cors");
+const path = require("path");
 const { Server } = require("socket.io");
 app.use(cors());
-app.use(express.static(path.join(__dirname, "./client/build")));
 app.use(express.static("public"));
 
 const server = http.createServer(app);
@@ -18,20 +18,22 @@ const io = new Server(server, {
 
 io.on("connection", (socket) => {
   console.log(`User Connected: ${socket.id}`);
-
+  
   socket.on("join_room", (data) => {
     socket.join(data);
     console.log(`User with ID: ${socket.id} joined room: ${data}`);
   });
-
+  
   socket.on("send_message", (data) => {
     socket.to(data.room).emit("receive_message", data);
   });
-
+  
   socket.on("disconnect", () => {
     console.log("User Disconnected", socket.id);
   });
 });
+
+app.use(express.static(path.join(__dirname, "./client/build")));
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build", "index.html"));
